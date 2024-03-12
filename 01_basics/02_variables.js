cbc75eb6afbc0481008b264345e962984039143d
devansh0810 <160541361+devansh0810@users.noreply.github.com>
2024-03-12 20:53:48 +0000

    variables js

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,10 @@
+const userId = 10000
+let emailId = "stidev@gmail.com"
+var number = 102
+let dob;
+accountcity = "nainital"
+console.log(userId)
+console.log(number)
+console.log(emailId)
+console.table([userId,emailId,number,dob])
+// we generally dont use var in javascript as it has some problems with understandins scope{}.
